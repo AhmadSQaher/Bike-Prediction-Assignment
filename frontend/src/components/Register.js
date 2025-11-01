146a287ce27cc398d07610d5ba2b625383d4d061
Ahmad Qaher <sqaher@my.centennialcollege.ca>
2025-11-01 19:37:24 -0400

    vercel fix ci eslint error by removing unused func

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -39,12 +39,6 @@ const Register = () => {
     setSocialOpen(true);
   };
 
-  const randomPassword = (len = 12) => {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=';
-    let out = '';
-    for (let i = 0; i < len; i++) out += chars[Math.floor(Math.random() * chars.length)];
-    return out;
-  };
 
   const handleSocialSubmit = async (e) => {
     e.preventDefault();
